a326ab6377efd03f038b1dfa3a6d1fd3a6f5879a
Dino Chioda <dchioda@me.com>
2020-04-03 14:26:54 -0400

    added country running totals for cases and deaths

--- models/corona/fct_corona.sql
+++ models/corona/fct_corona.sql
@@ -8,4 +8,13 @@ with facts as (
   from {{source('corona', 'cases')}}
 )
 
-select * from facts
+select
+  date,
+  country_code,
+  country_name,
+  cases as new_cases,
+  deaths as new_deaths,
+  sum(cases) over (partition by country_code order by date asc) as running_total_cases,
+  sum(deaths) over (partition by country_code order by date asc) as running_total_deaths
+from facts
+order by 2,1
