78503bf1fc6c598151f766624f0f6ef51ac6dc5f
Dino Chioda <dchioda@me.com>
2020-04-05 20:09:21 -0400

    added deaths_per_100k column

--- models/corona/fct_corona.sql
+++ models/corona/fct_corona.sql
@@ -3,6 +3,7 @@ with facts as (
     to_date(date,'DD/MM/YYYY') as date,
     country_code,
     REGEXP_REPLACE(country_name, '_', ' ', 'g') as country_name,
+    population_2018 as population,
     cases,
     deaths
   from {{source('corona', 'cases')}}
@@ -15,6 +16,7 @@ select
   cases as new_cases,
   deaths as new_deaths,
   sum(cases) over (partition by country_code order by date asc) as running_total_cases,
-  sum(deaths) over (partition by country_code order by date asc) as running_total_deaths
+  sum(deaths) over (partition by country_code order by date asc) as running_total_deaths,
+  100000.0*sum(deaths) over (partition by country_code order by date asc)/population as deaths_per_100k
 from facts
 order by 2,1
